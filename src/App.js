981a11dbafe34c9dcf6788290aac806da7c1a9b5
bryvl <bryan.val93@gmail.com>
2020-08-05 12:58:36 -0400

    removed default React logo and added placeholder array/prop during lunch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Container from "./components/Container";
 import Header from "./components/Header"
@@ -8,28 +7,16 @@ import ApiBtnDiv from "./components/ApiBtnDiv";
 import TeamFooter from './components/TeamFooter';
 
 function App() { 
+  let team = ["empty", "empty", "empty", "empty", "empty", "empty"];
+
   return (
     <div className="App">
       <Container>
         <Header></Header>
         <MainContent isRecruiting={false} />
         <ApiBtnDiv />
-        <TeamFooter />
+        <TeamFooter team={team}/>
       </Container>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
